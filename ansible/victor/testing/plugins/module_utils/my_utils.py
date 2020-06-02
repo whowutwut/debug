e9df1a21ac26be3e7d6e08aa0301cfe7d0ed06d9
Victor Hu <whowutwut@gmail.com>
2020-06-01 22:21:50 -0400

    Create a utils file

---
+++ ansible/victor/testing/plugins/module_utils/my_utils.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python 
+
+def helloWorld():
+    msg="Hello World Return Message"
+    print(msg) 
+    return(msg)
+
+def main():
+    helloWorld()
+
+if __name__ == "__main__":
+    main()
